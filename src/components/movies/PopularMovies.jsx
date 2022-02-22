428e4d0b7b15a8dff860259b7274212d16e11abd
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-22 18:04:08 +0100

    import removed

--- src/components/movies/PopularMovies.jsx
+++ src/components/movies/PopularMovies.jsx
@@ -1,11 +1,22 @@
 import React, { useEffect, useState } from "react";
 import MovieList from "./MovieList";
+import movieDB from "../../api/movieDB";
 
 const PopularMovies = () => {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {}, []);
-
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieDB.get("/popular", {
+        params: {
+          api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+        },
+      });
+      console.log(response.data.results);
+      setMovies(response.data.results);
+    };
+    fetchMovies();
+  }, []);
   return <MovieList movies={movies} />;
 };
 
