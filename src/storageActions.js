dc9b25f1f95907a6ec7951790a8e8081941f067f
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-22 18:09:32 +0100

    Fetch everything from storage function

---
+++ src/storageActions.js
@@ -0,0 +1,12 @@
+export const allStorage = () => {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while (i--) {
+        values.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+
+    return values;
+}
