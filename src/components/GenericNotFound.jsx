81617c5916fcdd005c2af38e25a2e9365a41350f
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-22 16:08:59 +0100

    GenericNotFound page

---
+++ src/components/GenericNotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GenericNotFound = () => {
+  return <h1 className="display-flex-style text margin-top">Page Not Found</h1>;
+};
+
+export default GenericNotFound;
