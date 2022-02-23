7a8927c6f1272a886a23ddd5a88ade407b239b53
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-23 15:19:53 +0100

    Added functions to storageActions

--- src/storageActions.js
+++ src/storageActions.js
@@ -10,3 +10,26 @@ export const allStorage = () => {
 
     return values;
 }
+
+export const getLogedInUser = () => {
+    return JSON.parse(localStorage.getItem('logedInUser'))
+}
+export const signUpUser = (firstName, lastName, email, password) => {
+    console.log('unutar local storage sign up', firstName, lastName, email, password)
+    localStorage.setItem('logedInUser', JSON.stringify({
+        email, password
+    }))
+    localStorage.setItem(`user${numOfLogedInUsers()}`, JSON.stringify({
+        firstName, lastName, email, password
+    }))
+}
+
+export const numOfLogedInUsers = () => {
+    return localStorage.length
+}
+
+export const logInUser = (email, password) => {
+    localStorage.setItem('logedInUser', JSON.stringify({
+        email, password
+    }))
+}
