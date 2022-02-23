76c2f96f6080576e85e4182ba329c6b42fbb6dbe
akunalic1 <72765733+akunalic1@users.noreply.github.com>
2022-02-23 15:18:28 +0100

    Validation to SignUp/LogIn form

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,14 +1,25 @@
-import React, { Component, createRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 import "../../css/auth.css";
+import { logInUser } from "../../storageActions";
 
-const validate = (values) => {
+const validate = (values, storageUsers) => {
   const errors = {};
   if (!values.email) {
     errors.email = "Required";
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = "Invalid email address";
+  } else if (
+    !storageUsers.filter((user) => user.email === values.email).length
+  ) {
+    errors.email = `User with email ${values.email} doesn't exist`;
+  } else if (
+    storageUsers
+      .filter((user) => user.email === values.email)
+      .filter((user) => user.password !== values.password).length
+  ) {
+    errors.password = "Wrong password";
   }
   if (!values.password) {
     errors.password = "Required";
@@ -16,68 +27,85 @@ const validate = (values) => {
   return errors;
 };
 
-const renderField = ({
-  input,
-  type,
-  label,
-  meta: { touched, error, warning },
-}) => (
-  <div>
-    <div>
-      <input {...input} placeholder={label} type={type} />
-      <div className="error-message">
-        {touched &&
-          ((error && <span>{error}</span>) ||
-            (warning && <span>{warning}</span>))}
-      </div>
-    </div>
-  </div>
-);
-
-const LoginForm = ({ storageUsers }) => {
+const LoginForm = ({ storageUsers, setUser }) => {
   const navigate = useNavigate();
-  const errorRef = createRef();
+  const [showIcon, setShowIcon] = useOutletContext();
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [errorValues, setErrorValues] = useState({});
+  const [submited, setSubmited] = useState(false);
 
-  const onLoginFormSubmit = (values) => {
-    if (storageUsers) {
-      if (!storageUsers.filter((user) => user.email === values.email).length) {
-        errorRef.current.textContent = `User with email ${values.email} doesn't exist`;
-      } else if (
-        storageUsers
-          .filter((user) => user.email === values.email)
-          .filter((user) => user.password !== values.password).length
-      ) {
-        errorRef.current.textContent = "Wrong password";
-      } else {
-        navigate("/auth/account");
-      }
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+    setSubmited(true);
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrorValues(validate(formValues, storageUsers));
+  };
+  useEffect(() => {
+    setShowIcon(true);
+    console.log(
+      "eeror values unutar useefect ",
+      errorValues,
+      " Object.keys(errorValues).length",
+      Object.keys(errorValues).length
+    );
+    if (Object.keys(errorValues).length === 0 && submited) {
+      console.log(formValues);
+      logInUser(formValues.email, formValues.password);
+      setUser({ email: formValues.email, password: formValues.password });
+      navigate("/");
+    }
+  }, [errorValues]);
+  const reset = () => {
+    setFormValues(initialValues);
+    setErrorValues({});
+    setSubmited(false);
+  };
+
   return (
     <div className="google-login-page margin-top">
       <form className="form glassmorphism" onSubmit={handleSubmit}>
-        <input
-          name="email"
-          type={"email"}
-          autoComplete="off"
-          className="without-border"
-        ></input>
-        <input
-          name="password"
-          type={"password"}
-          autoComplete="off"
-          className="without-border"
-        ></input>
-        <p ref={errorRef} className="error-message"></p>
+        <div>
+          <input
+            name="email"
+            type={"email"}
+            value={formValues.email}
+            onChange={handleChange}
+            autoComplete="on"
+            className="without-border"
+          ></input>
+          <p className="error-message">{errorValues.email}</p>
+        </div>
+        <div>
+          <input
+            name="password"
+            type={"password"}
+            value={formValues.password}
+            onChange={handleChange}
+            autoComplete="on"
+            className="without-border"
+          ></input>
+          <p className="error-message">{errorValues.password}</p>
+        </div>
+
         <button
           className="btn red-btn btn-full-w text-w-6 border"
           type="submit"
         >
           Log In
         </button>
-        <button className="btn black-btn btn-full-w text-w-6" type="button">
+        <button
+          className="btn black-btn btn-full-w text-w-6"
+          type="button"
+          onClick={reset}
+        >
           Clear Values
         </button>
       </form>
--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -1,9 +1,11 @@
-import React, { createRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { createRef, useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 import "../../css/auth.css";
+import { allStorage, signUpUser } from "../../storageActions";
 
-const validate = (values, storageUsers) => {
+const validate = (values) => {
+  const storageUsers = allStorage();
   const errors = {};
   if (!values.firstName) {
     errors.firstName = "Required";
@@ -27,63 +29,93 @@ const validate = (values, storageUsers) => {
   return errors;
 };
 
-const renderField = ({
-  input,
-  type,
-  label,
-  meta: { touched, error, warning },
-}) => (
-  <div>
-    <div style={{ textAlign: "center" }}>
-      <input {...input} placeholder={label} type={type} />
-      <div className="error-message">
-        {touched &&
-          ((error && <span>{error}</span>) ||
-            (warning && <span>{warning}</span>))}
-      </div>
-    </div>
-  </div>
-);
+const SignUpForm = ({ storageUsers, setUser }) => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [errorValues, setErrorValues] = useState({});
+  const [submited, setSubmited] = useState(false);
+  const [showIcon, setShowIcon] = useOutletContext();
 
-const SignUpForm = ({ storageUsers }) => {
-  const navigate = useNavigate();
-
-  const errorRef = createRef("");
-
-  const onSignUpFormSubmit = (values) => {
-    navigate("/auth/account");
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
   };
 
-  const handleSubmit = () => {};
-  const reset = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrorValues(validate(formValues));
+    setSubmited(true);
+  };
+  useEffect(() => {
+    setShowIcon(true);
+    if (Object.keys(errorValues).length === 0 && submited) {
+      console.log(formValues);
+      signUpUser(
+        formValues.firstName,
+        formValues.lastName,
+        formValues.email,
+        formValues.password
+      );
+      setUser({ email: formValues.email, password: formValues.password });
+    }
+  }, [errorValues]);
+  const reset = () => {
+    setFormValues(initialValues);
+    setErrorValues({});
+    setSubmited(false);
+  };
   return (
     <div className="google-signup-page margin-top">
       <form className="form glassmorphism" onSubmit={handleSubmit}>
-        <input
-          name="firstName"
-          type={"text"}
-          autoComplete="off"
-          className="without-border"
-        ></input>
-        <input
-          name="lastName"
-          type={"text"}
-          autoComplete="off"
-          className="without-border"
-        ></input>
-        <input
-          name="email"
-          type={"email"}
-          autoComplete="off"
-          className="without-border"
-        ></input>
-        <input
-          name="password"
-          type={"password"}
-          autoComplete="off"
-          className="without-border"
-        ></input>
-        <p ref={errorRef} className="error-message"></p>
+        <div>
+          <input
+            name="firstName"
+            type={"text"}
+            value={formValues.firstName}
+            onChange={handleChange}
+            autoComplete="on"
+            className="without-border"
+          ></input>
+          <p className="error-message">{errorValues.firstName}</p>
+        </div>
+        <div>
+          <input
+            name="lastName"
+            type={"text"}
+            autoComplete="on"
+            value={formValues.lastName}
+            onChange={handleChange}
+            className="without-border"
+          ></input>
+          <p className="error-message">{errorValues.lastName}</p>
+        </div>
+        <div>
+          <input
+            name="email"
+            type={"email"}
+            value={formValues.email}
+            onChange={handleChange}
+            autoComplete="on"
+            className="without-border"
+          ></input>
+          <p className="error-message">{errorValues.email}</p>
+        </div>
+        <div>
+          <input
+            name="password"
+            type={"password"}
+            autoComplete="on"
+            value={formValues.password}
+            onChange={handleChange}
+            className="without-border"
+          ></input>
+          <p className="error-message">{errorValues.password}</p>
+        </div>
         <button className="btn red-btn btn-full-w text-w-6" type="submit">
           Sign Up
         </button>
